3a70ac17b1d97a13d2736a1d8a3ab2b08e7959b8
Stanley <stanley05232930@gmail.com>
2024-06-26 16:30:23 +0800

    加個頁面

---
+++ src/pages/AuPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./AuPage.css";
+
+const AuPage = () => {
+    return (
+        <div className="au-page">
+            <h1>登入成功！</h1>
+            <p>歡迎回來，您已成功登入系統。</p>
+        </div>
+    );
+};
+
+export default AuPage;
